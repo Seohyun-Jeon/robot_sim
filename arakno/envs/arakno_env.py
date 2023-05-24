5fd56fa6a4dcea2432edfb3e5b66f701bdc8018d
antonella.rech <antonella.rech@studenti.unitn.it>
2023-05-25 00:18:52 +0900

    update

--- arakno/envs/arakno_env.py
+++ arakno/envs/arakno_env.py
@@ -1,3 +1,6 @@
+#render() which allow to visualize the agent in action. Note that graphical interface does not work on google colab, 
+#so we cannot use it directly (we have to rely on render_mode='rbg_array' to retrieve an image of the scene
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
@@ -8,36 +11,20 @@ import pybullet_data
 import math
 import numpy as np
 import random
+import time
 
 #TO DO:reward neagative or only positive ? 
-#
 
-#----------------------------------------------------------------#
-#GOAL of the ENV -> reach a specific endpoint in a plane 
-#in the fewest step possible ? or in the more stable way(cosider an uneven terrain)
-#----------------------------------------------------------------#
-
-import gym
-from gym import spaces
-from gym.utils import seeding
-
-import os
-import pybullet as p
-import pybullet_data
-
-import time
-import math
-import numpy as np
 
 class AraknoEnv(gym.Env):
-    metadata = {'render.modes': ['human', 'rgb_array']
+    metadata = {'render.modes': ['human', 'rgb_array'] #human, CONSOLE
     }
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def __init__(self, render = True):
+    def __init__(self, render = True): #True
         super(AraknoEnv, self).__init__()
 
         if (render):
@@ -60,7 +47,7 @@ class AraknoEnv(gym.Env):
         #load models
         self.plane = p.loadURDF("plane.urdf")
         
-        path_urdf = 'arakno/resources/urdfs/arakno.urdf'
+        path_urdf = 'arakno/resources/urdfs/arakno.urdf' 
         self.init_position = [0,0,0.25]
         self.init_orientation = p.getQuaternionFromEuler([0,0,0])
         self.araknoId = p.loadURDF(path_urdf, self.init_position, self.init_orientation)
@@ -97,7 +84,7 @@ class AraknoEnv(gym.Env):
 
         # Define the action space
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(12,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(49,), dtype=np.float32) #35
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(109,), dtype=np.float32) #49
     
     def reset(self):
         #reset the environment
@@ -173,47 +160,57 @@ class AraknoEnv(gym.Env):
     
     def compute_observation(self):
 
-        observation = []
-
-        n_joints = p.getNumJoints(self.araknoId)
-
-        #access to the current position and orientation of the base of the body 
-        base_pos, base_or = p.getBasePositionAndOrientation(self.araknoId)
-
-        observation.append(base_pos[0])
-        observation.append(base_pos[1])
-
-        observation.append(base_pos[2])
-        observation.append(base_or[0])
-        observation.append(base_or[1])
-        observation.append(base_or[2])
-        observation.append(base_or[3])
-
-        #take generalized coordinates
-        for i in range(n_joints):
-            joint_state = p.getJointState(self.araknoId, i)[0] # position
-            observation.append(joint_state)
-        
-        #access to linear and angular velocity of the base of a body 
-        base_v, base_w = p.getBaseVelocity(self.araknoId)
-        observation.append(base_v[0])
-        observation.append(base_v[1])
-        observation.append(base_v[2])
-        observation.append(base_w[0])
-        observation.append(base_w[1])
-        observation.append(base_w[2])
-
-        #take generalized velocities
-        for i in range(n_joints):
-            joint_state = p.getJointState(self.araknoId, i)[1] # velocity
-            observation.append(joint_state)
-
-        #take externals forces
+        baseOri = np.array(p.getBasePositionAndOrientation(self.araknoId))
         JointStates = p.getJointStates(self.araknoId, self.movingJoints)
-        for joint in JointStates:
-            observation.append(joint[2])
+        BaseAngVel = p.getBaseVelocity(self.araknoId)
 
-        return observation
+        obs = np.array([
+          baseOri[0][0],
+          baseOri[0][1],
+          baseOri[0][2],
+          # orientation (quarternion x,y,z,w) of the Torso -> 4
+          baseOri[1][0],
+          baseOri[1][1],
+          baseOri[1][2],
+          baseOri[1][3],
+          JointStates[0][0],
+          JointStates[1][0],
+          JointStates[2][0],
+          JointStates[3][0],
+          JointStates[4][0],
+          JointStates[5][0],
+          JointStates[6][0],
+          JointStates[7][0],
+          JointStates[8][0],
+          JointStates[9][0],
+          JointStates[10][0],
+          JointStates[11][0],
+          # 3-dim directional velocity and 3-dim angular velocity -> 3+3=6
+          BaseAngVel[0][0],
+          BaseAngVel[0][1],
+          BaseAngVel[0][2],
+          BaseAngVel[1][0],
+          BaseAngVel[1][1],
+          BaseAngVel[1][2],
+          JointStates[0][1],
+          JointStates[1][1],
+          JointStates[2][1],
+          JointStates[3][1],
+          JointStates[4][1],
+          JointStates[5][1],
+          JointStates[6][1],
+          JointStates[7][1],
+          JointStates[8][1],
+          JointStates[9][1],
+          JointStates[10][1],
+          JointStates[11][1]
+          ])
+        
+        external_forces = np.array([np.array(joint[2])
+                                  for joint in JointStates])
+        obs = np.append(obs, external_forces.ravel())
+        
+        return obs.tolist()
     
     def calculate_center_of_mass(self, robot_id):
         total_mass = 0
@@ -259,12 +256,12 @@ class AraknoEnv(gym.Env):
         stability = 1.0 / (1.0 + roll_diff + pitch_diff + yaw_diff)
 
         # forward_reward = (xposafter - xposbefore)
-        forward_reward = 20 * (xvelbefore - xvelafter)
+        forward_reward = 10 * (xvelbefore - xvelafter) #20
 
         #reward from applied torque to the motor in the last timestep
         JointStates = p.getJointStates(self.araknoId, self.movingJoints)
         torques = np.array([np.array(joint[3]) for joint in JointStates])
-        ctrl_cost = 1.0 * np.square(torques).sum()
+        ctrl_cost = 3.0 * np.square(torques).sum()
 
         #help the robot to detect when it has made contact with the ground, and provide 
         # information about the force and direction of the ground reaction forces
@@ -275,13 +272,13 @@ class AraknoEnv(gym.Env):
         # Penalize if the robot has fallen or is not alive
         alive = self.is_alive()
         if not alive:
-            alive_penalty = -1.0
-        else:
             alive_penalty = 0.0
+        else:
+            alive_penalty = 1.0
         
-        reward = forward_reward - ctrl_cost - contact_cost + alive_penalty + progress_diff + stability
+        reward = - ctrl_cost - contact_cost + alive_penalty + progress_diff + 5*stability + forward_reward 
 
-        #reward = reward if reward > 0 else 0
+        reward = reward if reward > 0 else 0
         
         return reward
     
@@ -291,10 +288,12 @@ class AraknoEnv(gym.Env):
         #2.fallen on the ground
         #3.maximum number timesteps reached
         done = False
-        (x,y,z), _ = p.getBasePositionAndOrientation(self.araknoId)
-        curr_pos = (x,y,z)
-        if (not self.is_alive()) or (self.envStepCounter >= 1000) :
+        curr_pos, _ = p.getBasePositionAndOrientation(self.araknoId)
+        if (not self.is_alive()):
             print("You Dead")
+            done = True
+        if (self.endpoint[0] - curr_pos[0]) < 0.01:
+            print("Arrived")
             done = True
 
         # Return whether the episode is done or not
@@ -304,7 +303,7 @@ class AraknoEnv(gym.Env):
         # Get the position and orientation of the robot's base
         pos, orn = p.getBasePositionAndOrientation(self.araknoId)
         #COM pos, height of body base => 0,15
-        ground_height = 0.087
+        ground_height = 0.09
         # Check if the center of mass (z-component) is above the ground
         if pos[2] > ground_height:
             return True
--- main_arakno.py
+++ main_arakno.py
@@ -10,73 +10,124 @@ import gym
 import numpy as np
 import time
 
-#env = gym.make('AntBulletEnv-v0')
-#env.render(mode="human")
+
+import gym
+import pybullet as p
+import numpy as np
+import pybullet_envs
+from stable_baselines3 import PPO, A2C, DQN
+import os
+
+import gym
+import numpy as np
+import time
+import pybullet_envs
+import panda_gym
+import gym
+
+import os
+
+from huggingface_sb3 import load_from_hub, package_to_hub
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+
+from huggingface_hub import notebook_login
+
 
 env = gym.make('arakno-v0')
-env.reset()
-#action = env.action_space.sample()
-while True:
-    env.render()
-    observation, reward, done, info = env.step(env.action_space.sample())
-    if done:
-        observation = env.reset()
-        time.sleep(1/30)
 
-print("ENDDDDDDDDDDDD")
+# n_cpu = 4
+# #total_timesteps = 200000000
+# total_timesteps = 200000
+# env = SubprocVecEnv([lambda: gym.make('arakno-v0') for i in range(n_cpu)])
+# model = PPO2(MlpPolicy, env, verbose=1)
+# model.learn(total_timesteps=total_timesteps)
+# model.save("experience_learned/ppo2_araknor_v0_testing")
+# del model # remove to demonstrate saving and loading
 
-"""# initialize environment and agent
-#env = gym.make("AntBulletEnv-v0")
-env = gym.make('arakno-v0')
-print(env.observation_space.shape[0], env.action_space.shape[0])
 
-#env.render()
+# env.reset()
+# while True:
+#     env.render()
+#     observation, reward, done, info = env.step(env.action_space.sample())
+#     if done:
+#         observation = env.reset()
+#         time.sleep(1/30)
 
-# create RL model
+# print("ENDDDDDDDDDDDD")
+
+#Instantiate the env
+vec_env = make_vec_env(env, n_envs=1)
+env = SubprocVecEnv([lambda: gym.make('arakno-v0') for i in range(4)])
+env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+
 model = A2C("MlpPolicy", env, verbose=1)
 
-# train model on environment
-model.learn(total_timesteps=int(1e6))
+# Train the agent
+model.learn(20000)#200000000
 
-# test model on environment
-obs = env.reset()
+# Save the model and  VecNormalize statistics when saving the agent
+model.save("a2c-AraknoEnv-v0")
+env.save("vec_normalize.pkl")
 
-for i in range(1000):
-    action, _ = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        obs = env.reset()
-        time.sleep(1/30)
-for step in range(1000):
-        env.render()
-        action = env.action_space.sample()
-        #env.apply_action(action)
-        #p.setJointMotorControl2(env.araknoId, 0, p.POSITION_CONTROL, targetPosition=action[0])
-        #p.stepSimulation()
-        #print("action: ", action)
-        obs, reward, done, info = env.step(action)
-        if done:
-            print(f'Done at step {step}')
-            obs = env.reset()
-env.close()
+# # initialize environment and agent
+# env = gym.make('arakno-v0')
+# #env.render()
 
+# # create RL model
+# model = A2C("MlpPolicy", env, verbose=1)
 
-# Start the state logging
-#log_id = p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "temp_video.mp4")
+# # train model on environment
+# model.learn(total_timesteps=int(1e6))
 
-env.reset()
-#env.step(env.action_space.sample())
-for i in range(1000):
-    #to learn to walk on a flat plane-> the camera is not needed for now
-    env.render()
-    #TO DO: step reward action
-    #    obs, rewards, dones, info = env.step(action)
-    env.render()
-#    env.step(env.action_space.sample()) # take a random action
-    p.stepSimulation()    
+# # test model on environment
+# obs = env.reset()
 
-# Stop the state logging
-#p.stopStateLogging(log_id)
+# for i in range(1000):
+#     action, _ = model.predict(obs, deterministic=True)
+#     obs, reward, done, info = env.step(action)
+#     env.render()
+#     if done:
+#         print(f'Done at step {step}')
+#         obs = env.reset()
+#         time.sleep(1/30)
 
-env.close()"""
+#evaluate the performance
+# from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+# import gym
+# import pybullet as p
+# import numpy as np
+# import pybullet_envs
+# from stable_baselines3 import PPO, A2C, DQN
+# import os
+
+# import gym
+# import numpy as np
+# import time
+
+# # # # Enjoy trained agent
+# # Load the agent
+# model = A2C.load("a2c-AraknoEnv-v0")
+# env = DummyVecEnv([env])
+# eval_env = VecNormalize.load("vec_normalize.pkl", env)
+# #  do not update them at test time
+# eval_env.training = False
+# # reward normalization is not needed at test time
+# eval_env.norm_reward = False
+
+# obs = eval_env.reset()
+
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, info = env.step(action)
+#     eval_env.render()
+
+# Load the saved statistics
+#eval_env = DummyVecEnv([env])
+
+#mean_reward, std_reward = evaluate_policy(model, env)
+
+#print(f"Mean reward = {mean_reward:.2f} +/- {std_reward:.2f}")
